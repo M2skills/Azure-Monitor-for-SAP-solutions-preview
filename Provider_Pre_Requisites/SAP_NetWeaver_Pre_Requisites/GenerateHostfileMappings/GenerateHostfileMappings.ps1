5fad488934683a1d143abc77216ec260c2189cc8
Devansh Jain <86314060+devanshjainms@users.noreply.github.com>
2023-05-03 17:04:51 -0700

    added try-catch block to handle exceptions

--- Provider_Pre_Requisites/SAP_NetWeaver_Pre_Requisites/GenerateHostfileMappings/GenerateHostfileMappings.ps1
+++ Provider_Pre_Requisites/SAP_NetWeaver_Pre_Requisites/GenerateHostfileMappings/GenerateHostfileMappings.ps1
@@ -10,18 +10,40 @@ param(
 # Set the path to the SAP hostctrl executable
 Set-Location -Path "C:\Program Files\SAP\hostctrl\exe"
 
-# Get the hostnames of the SAP system instance
-$hosts = .\sapcontrol -prot NI_HTTP -nr $instanceNumber -format script -function GetSystemInstanceList | Select-String -Pattern "hostname" | ForEach-Object {$_.Line.Split()[2]}
+try {
+    # Get the hostnames of the SAP system instance
+    $hosts = .\sapcontrol -prot NI_HTTP -nr $instanceNumber -format script -function GetSystemInstanceList | Select-String -Pattern "hostname" | ForEach-Object {$_.Line.Split()[2]}
 
-# Get the fully qualified domain name
-$fqdn = .\sapcontrol -prot NI_HTTP -nr $instanceNumber -format script -function ParameterValue | Select-String -Pattern "SAPFQDN" | ForEach-Object {$_.Line.Split("=")[1]}
+    # Get the fully qualified domain name
+    $fqdn = .\sapcontrol -prot NI_HTTP -nr $instanceNumber -format script -function ParameterValue | Select-String -Pattern "SAPFQDN" | ForEach-Object {$_.Line.Split("=")[1]}
+}
+catch {
+    Write-Host "Failed to get the hostnames of the SAP system instance and the fully qualified domain name"
+    Exit 1
+}
+
+if ($hosts.Count -eq 0) {
+    Write-Host "Failed to get the hostnames of the SAP system instance"
+    Exit 1
+}
+if ($fqdn.Length -eq 0) {
+    Write-Host "Failed to get the fully qualified domain name"
+    Exit 1
+}
+
 
 $hostfile_entries = New-Object System.Collections.Generic.HashSet[string]
 
 # Loop through each hostnames
 foreach ($hostname in $hosts) {
-    $ping = Test-Connection -ComputerName $hostname -Count 1
-    $ip = $ping.IPV4Address.IPAddressToString
+    try {
+        $ping = Test-Connection -ComputerName $hostname -Count 1
+        $ip = $ping.IPV4Address.IPAddressToString
+    }
+    catch {
+        Write-Host "Failed to get the IP address of the hostname $hostname"
+        Exit 1
+    }
     $hostfile_entries.add("$ip" + " " + "$hostname" + ".$fqdn" + " " + "$hostname")
 }
 
--- Provider_Pre_Requisites/SAP_NetWeaver_Pre_Requisites/GenerateHostfileMappings/GenerateHostfileMappings.sh
+++ Provider_Pre_Requisites/SAP_NetWeaver_Pre_Requisites/GenerateHostfileMappings/GenerateHostfileMappings.sh
@@ -10,11 +10,35 @@ instanceNumber=00
 # Set the path to the SAP hostctrl executable
 cd "/usr/sap/hostctrl/exe"
 
-# Get the hostnames of the SAP system instance
-hosts=$(./sapcontrol -prot PIPE -nr $instanceNumber -format script -function GetSystemInstanceList | grep "hostname" | cut -d " " -f 3)
+hosts=""
+fqdn=""
 
-# Get the fully qualified domain name
-fqdn=$(./sapcontrol -prot PIPE -nr $instanceNumber -format script -function ParameterValue | grep "SAPFQDN" | cut -d "=" -f 2 | tr -d '\r')
+function get_hosts() {
+    # Get the hostnames of the SAP system instance
+    hosts=$(./sapcontrol -prot PIPE -nr $instanceNumber -format script -function GetSystemInstanceList | grep "hostname" | cut -d " " -f 3)
+}
+
+function get_fqdn() {
+    # Get the fully qualified domain name
+    fqdn=$(./sapcontrol -prot PIPE -nr $instanceNumber -format script -function ParameterValue | grep "SAPFQDN" | cut -d "=" -f 2 | tr -d '\r')
+}
+
+{
+    get_hosts
+    get_fqdn
+} || {
+    echo "Failed to get the hostnames and fully qualified domain name. Please check if the instance number on line 8 is correct."
+}
+
+if [ -z "$hosts" ]; then
+    echo "Failed to get the hostnames. Please check if the instance number on line 8 is correct."
+    exit 1
+fi
+
+if [ -z "$fqdn" ]; then
+    echo "Failed to get the fully qualified domain name. Please check if the instance number on line 8 is correct."
+    exit 1
+fi
 
 # Declare an array to store the host file entries
 hostfile_entries=()
@@ -22,7 +46,14 @@ hostfile_entries=()
 # Loop through each hostname
 for hostname in $hosts
 do
-    ip=$(ping -c 1 $hostname | head -n 1 | cut -d "(" -f 2 | cut -d ")" -f 1)
+    # Get the IP address of the hostname
+    ip=""
+    { 
+        ip=$(ping -c 1 $hostname | head -n 1 | cut -d "(" -f 2 | cut -d ")" -f 1)
+    } || {
+        echo "Failed to get the IP address of $hostname ${?}"
+        exit 1
+    }
     hostfile_entries+="$ip $hostname.$fqdn $hostname,"
 done
 
